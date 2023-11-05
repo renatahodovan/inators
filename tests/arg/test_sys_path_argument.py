2d8da974a0bfa9cd3b3b284196f61bfbbe7e474e
Akos Kiss <akiss@inf.u-szeged.hu>
2023-11-05 22:50:27 +0100

    Ensure test quality via linting
    - Change test folders to packages to allow relative imports.
    - List standard modules first in imports.
    - Don't specify object as superclass.
    - Avoid u string-prefix.
    - Use double blank lines to separate top-level elements.

--- tests/arg/test_sys_path_argument.py
+++ tests/arg/test_sys_path_argument.py
@@ -1,16 +1,17 @@
-# Copyright (c) 2021 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2021-2023 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
 # This file may not be copied, modified, or distributed except
 # according to those terms.
 
-import pytest
 import sys
+
+import pytest
 
 from inators import arg as inators_arg
 
-from common_arg import _test_add_argument, MockNamespace
+from .common_arg import _test_add_argument, MockNamespace
 
 
 @pytest.mark.parametrize('func_args, func_kwargs, sys_argv, exp', [
